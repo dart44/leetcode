70c13ce5cf97a200533d7228eb0589bf64dde691
Jeremy Crook <jeremy.crook2@gmail.com>
2022-05-13 06:50:20 -0400

    added 680

---
+++ 680-Valid-Palindrome-II.py
@@ -0,0 +1,60 @@
+import logging
+# logging.basicConfig(level=logging.DEBUG)
+
+def reverse_string(str):
+        reverse = ""
+        for i in str:
+            reverse = i + reverse
+        return reverse
+    
+def format_palindrome(_string):
+        fs = ''.join(filter(str.isalnum, _string))
+        fs = str.lower(fs)
+        return fs
+    
+def sort_string(_s):
+    sorted_characters = sorted(_s)
+    _s = ''.join(sorted_characters)
+    return _s
+
+class Solution:
+    # Determine if a string is a palindrome
+    def validPalindrome(self, s) -> bool:
+            
+        i = 0
+        j = len(s) - 1
+        
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return self.helper(s, i + 1, j) or self.helper(s, i, j - 1)                
+        return True
+    
+    def helper(self, s, left, right):
+        
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        
+        return True
+
+        
+# Test script
+def main():
+    input_value = input('Enter test string: ')
+    res = Solution()
+    # print(res.validPalindrome("tebbem")) #false
+    # print(res.validPalindrome("abca")) #true
+    # print(res.validPalindrome("abc")) #false
+    # print(res.validPalindrome("aasdasdasfdsgdsggdfghdfsgdf")) #false
+    print(res.validPalindrome(input_value))
+    return res.validPalindrome(input_value)
+
+# Run test script if this file is not being imported
+if __name__ == '__main__':
+    main()
