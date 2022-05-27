1459f1485dcc541c4a32dc263f0499892af9ada6
Jeremy Crook <jeremy.crook2@gmail.com>
2022-05-27 15:37:54 -0400

    More Pythonic

--- 167-Two-Sum-II.py
+++ 167-Two-Sum-II.py
@@ -1,18 +1,18 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
         dict = {}
-        for i in range(len(numbers)):
-            complement = target - numbers[i]
+        for i, number in enumerate(numbers, 1):
+            complement = target - number
             if (dict.get(complement) != None):
-                return (dict.get(complement), i+1)
-            dict.update( {numbers[i]: i+1} )
+                return (dict.get(complement), i)
+            dict.update( {number: i} )
 
 # Test
 def main():
     test = Solution()
     n = [0, 0, 3, 5]
     t = 5
-    print(f'{test.twoSum(n, t)}') # (1, 4)
+    print(f'{test.twoSum(n, t)}') # (2, 4)
     
 # Run test script if this file is not being imported
 if __name__ == '__main__':
