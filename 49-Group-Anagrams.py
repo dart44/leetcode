58166bb205b033c6dc40d1949b6618d7b7048e2c
Jeremy Crook <jeremy.crook2@gmail.com>
2022-05-27 21:08:37 -0400

    49

---
+++ 49-Group-Anagrams.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        dict = defaultdict(list)
+        for word in strs:
+            letters = list(word)
+            sl = sorted(letters)
+            sorted_letters = ''.join(sl)
+            dict[sorted_letters].append(word)
+        return list(dict.values())
+    
+# Test
+def main():
+    test = Solution()
+    n = ["eat","tea","tan","ate","nat","bat"]
+    print(test.groupAnagrams(n))
+    
+# Run test script if this file is not being imported
+if __name__ == '__main__':
+    main()
