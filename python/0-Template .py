c3f042eb7fc76fcca5d445d5f65370c9e41b78f7
Jeremy Crook <jeremy.crook2@gmail.com>
2022-06-13 11:35:05 -0400

    minor python changes

--- python/0-Template .py
+++ python/0-Template .py
@@ -2,12 +2,12 @@ class Solution:
     def name():
         return 0
 
-# Test
+# Driver Code
 def main():
     test = Solution()
     n = 0
     print(f'{test.name()}')
     
-# Run test script if this file is not being imported
+# Run driver code if this file is not being imported
 if __name__ == '__main__':
     main()
--- python/167-Two-Sum-II.py
+++ python/167-Two-Sum-II.py
@@ -7,14 +7,14 @@ class Solution:
                 return (dict.get(complement), i)
             dict.update( {number: i} )
 
-# Test
+# Driver Code
 def main():
     test = Solution()
     n = [0, 0, 3, 5]
     t = 5
     print(f'{test.twoSum(n, t)}') # (2, 4)
     
-# Run test script if this file is not being imported
+# Run driver code if this file is not being imported
 if __name__ == '__main__':
     # profile.run('main()') - test execution time
     main()
