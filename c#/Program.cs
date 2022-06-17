21bd72193d75c59d2632a27d29ca1d6196dce623
Jeremy Crook <jeremy.crook2@gmail.com>
2022-06-16 22:24:33 -0400

    c sharp

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string hello = "Hello World";
-Console.WriteLine("Hello, World!");
+
+namespace CSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      void HelloWorld()
+      {
+        string hello = "Hello World";
+        Console.WriteLine(hello);
+        List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+        Console.WriteLine(list[0]);
+      }
+      HelloWorld();
+    }
+  }
+}
