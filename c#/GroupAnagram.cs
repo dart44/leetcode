23d14e00dccc2a226477129152b123e504038d5e
Jeremy Crook <jeremy.crook2@gmail.com>
2022-06-18 10:00:07 -0400

    csharp

--- c#/GroupAnagram.cs
+++ c#/GroupAnagram.cs
@@ -1,23 +1,23 @@
-namespace GroupAnagram;
+// namespace GroupAnagram;
 
-public class Solution
-{
-  public IList<IList<string>> GroupAnagrams(string[] strs)
-  {
-    Dictionary<string, IList> keyValuePairs = new Dictionary<string, IList>();
-    for (int i = 0; i < strs.Length; i++)
-    {
-      char[] chars = strs[i].ToCharArray();
-      Array.Sort(chars);
-      string sorted_letters = new string(chars);
-      if (keyValuePairs.ContainsKey(sorted_letters))
-      {
-        keyValuePairs[sorted_letters].Add(strs[i]);
-      }
-      else
-      {
-        keyValuePairs.Add(sorted_letters, new List { strs[i] });
-      }
-    }
-  }
-}
+// public class Solution
+// {
+//   public IList<IList<string>> GroupAnagrams(string[] strs)
+//   {
+//     Dictionary<string, IList> keyValuePairs = new Dictionary<string, IList>();
+//     for (int i = 0; i < strs.Length; i++)
+//     {
+//       char[] chars = strs[i].ToCharArray();
+//       Array.Sort(chars);
+//       string sorted_letters = new string(chars);
+//       if (keyValuePairs.ContainsKey(sorted_letters))
+//       {
+//         keyValuePairs[sorted_letters].Add(strs[i]);
+//       }
+//       else
+//       {
+//         keyValuePairs.Add(sorted_letters, new List { strs[i] });
+//       }
+//     }
+//   }
+// }
